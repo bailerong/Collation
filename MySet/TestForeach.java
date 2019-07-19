f741b635808f74626b5eb4ce8d8a3bd931b47581
bailerong <38148426+bailerong@users.noreply.github.com>
2019-07-19 21:39:35 +0800

    Update TestForeach.java

--- MySet/TestForeach.java
+++ MySet/TestForeach.java
@@ -1,8 +1,6 @@
 package MySet;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class TestForeach {
     public static void main(String[] args) {
         List<String> list=new ArrayList<>();
