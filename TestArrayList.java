f618013da88c62e2c80bcf8cf9ad23b1eb3aa123
bailerong <38148426+bailerong@users.noreply.github.com>
2019-08-05 21:31:32 +0800

    Update TestArrayList.java

--- TestArrayList.java
+++ TestArrayList.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class TestArrayList {
     public static void main(String[] args) {
-        List<String> list=new ArrayList<>();
+        List<String> list=new ArrayList<>();//作为我们的List接口的三大子类之一
         System.out.println(list.size()+"/"+list.isEmpty());
         list.add("hello");
         list.add("hello");
